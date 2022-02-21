e6840f2c8f813e01852487194b40586320f6ef38
Canwaiting <455021735@qq.com>
2022-02-21 16:57:04 +0800

    1

--- log/block_queue.h
+++ log/block_queue.h
@@ -25,6 +25,7 @@ public:
         }
 
         m_max_size = max_size;
+        //阻塞队列的string数组
         m_array = new T[max_size];
         m_size = 0;
         m_front = -1;
@@ -48,6 +49,7 @@ public:
 
         m_mutex.unlock();
     }
+
     //判断队列是否满了
     bool full() 
     {
@@ -61,6 +63,7 @@ public:
         m_mutex.unlock();
         return false;
     }
+
     //判断队列是否为空
     bool empty() 
     {
@@ -73,6 +76,7 @@ public:
         m_mutex.unlock();
         return false;
     }
+
     //返回队首元素
     bool front(T &value) 
     {
@@ -86,6 +90,7 @@ public:
         m_mutex.unlock();
         return true;
     }
+
     //返回队尾元素
     bool back(T &value) 
     {
@@ -121,6 +126,7 @@ public:
         m_mutex.unlock();
         return tmp;
     }
+
     //往队列添加元素，需要将所有使用队列的线程先唤醒
     //当有元素push进队列,相当于生产者生产了一个元素
     //若当前没有线程等待条件变量,则唤醒无意义
@@ -145,6 +151,7 @@ public:
         m_mutex.unlock();
         return true;
     }
+
     //pop时,如果当前队列没有元素,将会等待条件变量
     bool pop(T &item)
     {
