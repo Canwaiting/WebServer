45a6f296d310810013fb8466022bbc8fb1cac1b9
Canwaiting <455021735@qq.com>
2022-02-21 15:16:07 +0800

    1

--- timer/lst_timer.cpp
+++ timer/lst_timer.cpp
@@ -6,6 +6,7 @@ sort_timer_lst::sort_timer_lst()
     head = NULL;
     tail = NULL;
 }
+//常规销毁链表
 sort_timer_lst::~sort_timer_lst()
 {
     util_timer *tmp = head;
@@ -17,6 +18,7 @@ sort_timer_lst::~sort_timer_lst()
     }
 }
 
+//添加定时器，内部调用私有成员add_timer
 void sort_timer_lst::add_timer(util_timer *timer)
 {
     if (!timer)
@@ -28,6 +30,8 @@ void sort_timer_lst::add_timer(util_timer *timer)
         head = tail = timer;
         return;
     }
+    //如果新的定时器超时时间小于当前头部结点
+    //直接将当前定时器结点作为头部结点
     if (timer->expire < head->expire)
     {
         timer->next = head;
@@ -35,8 +39,11 @@ void sort_timer_lst::add_timer(util_timer *timer)
         head = timer;
         return;
     }
+    //否则调用私有成员，调整内部结点
     add_timer(timer, head);
 }
+
+//调整定时器，任务发生变化时，调整定时器在链表中的位置
 void sort_timer_lst::adjust_timer(util_timer *timer)
 {
     if (!timer)
@@ -44,10 +51,13 @@ void sort_timer_lst::adjust_timer(util_timer *timer)
         return;
     }
     util_timer *tmp = timer->next;
+    //被调整的定时器在链表尾部
+    //定时器超时值仍然小于下一个定时器超时值，不调整
     if (!tmp || (timer->expire < tmp->expire))
     {
         return;
     }
+    //被调整定时器是链表头结点，将定时器取出，重新插入
     if (timer == head)
     {
         head = head->next;
@@ -55,6 +65,7 @@ void sort_timer_lst::adjust_timer(util_timer *timer)
         timer->next = NULL;
         add_timer(timer, head);
     }
+    //被调整定时器在内部，将定时器取出，重新插入
     else
     {
         timer->prev->next = timer->next;
@@ -62,12 +73,15 @@ void sort_timer_lst::adjust_timer(util_timer *timer)
         add_timer(timer, timer->next);
     }
 }
+
+//删除定时器
 void sort_timer_lst::del_timer(util_timer *timer)
 {
     if (!timer)
     {
         return;
     }
+    //链表中只有一个定时器，需要删除该定时器
     if ((timer == head) && (timer == tail))
     {
         delete timer;
@@ -75,6 +89,7 @@ void sort_timer_lst::del_timer(util_timer *timer)
         tail = NULL;
         return;
     }
+    //被删除的定时器为头结点
     if (timer == head)
     {
         head = head->next;
@@ -82,6 +97,7 @@ void sort_timer_lst::del_timer(util_timer *timer)
         delete timer;
         return;
     }
+    //被删除的定时器为尾结点
     if (timer == tail)
     {
         tail = tail->prev;
@@ -89,26 +105,34 @@ void sort_timer_lst::del_timer(util_timer *timer)
         delete timer;
         return;
     }
+    //被删除的定时器在链表内部，常规链表结点删除
     timer->prev->next = timer->next;
     timer->next->prev = timer->prev;
     delete timer;
 }
+
 void sort_timer_lst::tick()
 {
     if (!head)
     {
         return;
     }
-    
+    //获取当前时间
     time_t cur = time(NULL);
     util_timer *tmp = head;
+
+    //遍历定时器链表
     while (tmp)
     {
+        //链表容器为升序排列
+        //当前时间小于定时器的超时时间，后面的定时器也没有到期
         if (cur < tmp->expire)
         {
             break;
         }
+        //当前定时器到期，则调用回调函数，执行定时事件
         tmp->cb_func(tmp->user_data);
+        //将处理后的定时器从链表容器中删除，并重置头结点
         head = tmp->next;
         if (head)
         {
@@ -119,10 +143,12 @@ void sort_timer_lst::tick()
     }
 }
 
+//主要用于调整链表内部结点
 void sort_timer_lst::add_timer(util_timer *timer, util_timer *lst_head)
 {
     util_timer *prev = lst_head;
     util_timer *tmp = prev->next;
+    //遍历当前结点之后的链表，按照超时时间找到目标定时器对应的位置，常规双向链表插入操作
     while (tmp)
     {
         if (timer->expire < tmp->expire)
@@ -136,6 +162,7 @@ void sort_timer_lst::add_timer(util_timer *timer, util_timer *lst_head)
         prev = tmp;
         tmp = tmp->next;
     }
+    //遍历完发现，目标定时器需要放到尾结点处
     if (!tmp)
     {
         prev->next = timer;
@@ -188,24 +215,33 @@ void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
 }
 
 //信号处理函数
+//信号处理函数中仅仅通过管道发送信号值，不处理信号对应的逻辑，缩短异步执行时间，减少对主程序的影响。
 void Utils::sig_handler(int sig)
 {
     //为保证函数的可重入性，保留原来的errno
+    //可重入性表示中断后再次进入该函数，环境变量与之前相同，不会丢失数据
     int save_errno = errno;
     int msg = sig;
+    //将信号值从管道写端写入，传输字符类型，而非整型
     send(u_pipefd[1], (char *)&msg, 1, 0);
+    //将原来的errno赋值为当前的errno
     errno = save_errno;
 }
 
 //设置信号函数
+//项目中设置信号函数，仅关注SIGTERM和SIGALRM两个信号
 void Utils::addsig(int sig, void(handler)(int), bool restart)
 {
+    //创建sigaction结构体变量
     struct sigaction sa;
     memset(&sa, '\0', sizeof(sa));
+    //信号处理函数中仅仅发送信号值，不做对应逻辑处理
     sa.sa_handler = handler;
     if (restart)
         sa.sa_flags |= SA_RESTART;
+    //将所有信号添加到信号集中
     sigfillset(&sa.sa_mask);
+    //执行sigaction函数
     assert(sigaction(sig, &sa, NULL) != -1);
 }
 
@@ -224,6 +260,7 @@ void Utils::show_error(int connfd, const char *info)
 
 int *Utils::u_pipefd = 0;
 int Utils::u_epollfd = 0;
+
 
 class Utils;
 void cb_func(client_data *user_data)
